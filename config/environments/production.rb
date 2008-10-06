acca224ce7cfcfc7c98f0034613bb058e2307e8b
Dmitry Sokurenko <dmitry@sokurenko.name>
2008-10-06 19:53:01 +0400

    Add Google Analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,3 +2,5 @@ config.log_level = :info
 config.cache_classes = true
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
+
+GOOGLE_ANALYTICS_ID = "UA-1612812-2"
