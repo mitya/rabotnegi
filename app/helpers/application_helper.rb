0461c5795b69e187d8434359c3c2794a56f864ba
Dmitry Sokurenko <dmitry@sokurenko.name>
2008-12-25 21:40:01 +0300

    Fix localization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,10 +15,10 @@ module ApplicationHelper
   def errors_for(object, options = {})
     return '' if object.errors.empty?
     
-    header_message = options.delete(:header_message) || translate("active_record.errors.template.default_header")
+    header_message = options.delete(:header_message) || translate("activerecord.errors.template.header")
     error_messages = []
     object.errors.each do |attr, msg|
-      attr_name = translate("active_record.human_attribute_names.#{object.class.name.underscore}.#{attr}")
+      attr_name = translate("activerecord.attributes.#{object.class.name.underscore}.#{attr}")
       error_messages << content_tag(:li, content_tag(:b, attr_name) + ' — ' + msg)
     end
     
