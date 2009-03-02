6e046039a6eecb2a55bfe993884fe7635db85f73
Dmitry Sokurenko <dmitry@sokurenko.name>
2009-03-02 20:32:24 +0300

    Fix log level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-config.log_level = :info
+config.log_level = :debug
 config.cache_classes = true
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
