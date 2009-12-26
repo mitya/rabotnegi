6feb52e595bb2638189fc371bd0da34feed689cc
Dmitry Sokurenko <dmitry@sokurenko.name>
2009-12-26 21:37:45 +0300

    Migrate Rails to 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,9 @@
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.action_controller.session = { :session_key => "_jobs_session", :secret => "4b92cb861beba0fa9d81a00f7c427cc23acf164fd9abbd2d542ea4f3352c59f830212504902ba2ab6ad082317e244f8b21c6055e541a618ec485e8f5493f2085" }
- 	config.gem 'mislav-will_paginate', :version => '~> 2.3.8', :lib => 'will_paginate', :source => 'http://gems.github.com'
+ 	config.gem 'will_paginate'
   config.gem 'json', :lib => 'json/add/rails'
   config.time_zone = 'Moscow'
   config.action_controller.cache_store = :file_store, "tmp/cache"
