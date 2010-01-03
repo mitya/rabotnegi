4ef8002bdc1dd628e9945183f9be6de7f859a50f
Dmitry Sokurenko <dmitry@sokurenko.name>
2010-01-03 19:21:00 +0300

    Use constants for select option lists and migrate to standard grouped_options_for_select

--- app/helpers/model_helper.rb
+++ app/helpers/model_helper.rb
@@ -1,27 +1,21 @@
 module ModelHelper
-  def industry_groups
-    @@for_select_with_groups ||= [
-      ['Популярные', Industry.popular.map { |industry| [industry.name, industry.code.to_s] }],
-      ['Остальные', Industry.other.map { |industry| [industry.name, industry.code.to_s] }]
-    ]
-  end
+  IndustryOptions = [
+    ['Популярные', Industry.popular.map { |industry| [industry.name, industry.code.to_s] }],
+    ['Остальные', Industry.other.map { |industry| [industry.name, industry.code.to_s] }]
+  ]  
+  
+  SalaryOptions = [
+  	['до 10 000', -10000],
+  	['до 20 000', -20000],
+  	['до 30 000', -30000],
+  	['до 40 000', -40000],
+  	['до 50 000', -50000],
+  	['от 10 000',  10000],
+  	['от 20 000',  20000],
+  	['от 30 000',  30000],
+  	['от 40 000',  40000],
+  	['от 50 000',  50000]
+  ]
 
-  def cities
-    @@cities ||= City.all.map { |city| [city.name, city.code.to_s] }
-  end
-
-  def salaries
-    @@salaries ||= [
-    	['до 10 000', -10000],
-    	['до 20 000', -20000],
-    	['до 30 000', -30000],
-    	['до 40 000', -40000],
-    	['до 50 000', -50000],
-    	['от 10 000',  10000],
-    	['от 20 000',  20000],
-    	['от 30 000',  30000],
-    	['от 40 000',  40000],
-    	['от 50 000',  50000]
-    ]
-  end
+  CityOptions = City.all.map { |city| [city.name, city.code.to_s] }
 end
--- lib/ext/rails.rb
+++ lib/ext/rails.rb
@@ -1,8 +1,5 @@
 class ActiveRecord::Base
-end
-
-class << ActiveRecord::Base
-  def property(*args)
+  def self.property(*args)
   end
 end
 
@@ -19,29 +16,23 @@ class ActionView::Base
 end
 
 module ActionView::Helpers::FormOptionsHelper
-  def option_groups_for_select(container, selected = nil)
-    option_groups_from_collection_for_select(container, :last, :first, :last, :first, selected)
-  end
-
-  # Generates a SELECT tag with option groups.
-  # Groups structure is: [[grplabel, [optlabel, opt]], ...]
-  def group_select(object, method, groups, options = {}, html_options = {})
-    ActionView::Helpers::InstanceTag.new(object, method, self, options.delete(:object)).to_group_select_tag(groups, options, html_options)
+  def grouped_select(object, method, groups, options = {}, html_options = {})
+    ActionView::Helpers::InstanceTag.new(object, method, self, options.delete(:object)).to_grouped_select_tag(groups, options, html_options)
   end
 end
 
 class ActionView::Helpers::InstanceTag
-  def to_group_select_tag(groups, options, html_options)
+  def to_grouped_select_tag(groups, options, html_options)
     html_options = html_options.stringify_keys
     add_default_name_and_id(html_options)
     value = value(object)
     selected_value = options.has_key?(:selected) ? options[:selected] : value
-    content_tag("select", add_options(@template_object.option_groups_for_select(groups, selected_value), options, selected_value), html_options)
+    content_tag("select", add_options(@template_object.grouped_options_for_select(groups, selected_value), options, selected_value), html_options)
   end
 end
 
 class ActionView::Helpers::FormBuilder
-  def group_select(method, choices, options = {}, html_options = {})
-    @template.group_select(@object_name, method, choices, objectify_options(options), @default_options.merge(html_options))
+  def grouped_select(method, choices, options = {}, html_options = {})
+    @template.grouped_select(@object_name, method, choices, objectify_options(options), @default_options.merge(html_options))
   end
 end
