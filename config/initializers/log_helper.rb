5e68ab612a5fda9d2fea6aaf568e83a93f35140b
Dmitry Sokurenko <dmitry@sokurenko.name>
2010-01-03 19:35:31 +0300

    Refactor the log helper

--- config/initializers/log_helper.rb
+++ config/initializers/log_helper.rb
@@ -2,12 +2,17 @@ def assert(condition, message = "Assertion failed")
   condition || raise(mesage)
 end
 
-def w(object, comment = nil)
-  comment &&= "#{comment}: "
+# __w(customer.name, "Customer") => /// Customer = "Joe"
+# __w(customer.name) => /// "Joe"
+def __w(object, comment = nil)
+  comment &&= "#{comment} = "
   inspection = PP.pp(object, StringIO.new, 100).string rescue object.inspect
   Rails.logger.debug("/// #{comment}#{inspection}")
 end
 
-def d(expression, binding = nil)
-  l(eval(expression.to_s, binding), expression.to_s)
+# __d(customer.name, binding) => /// customer.name = "Joe"
+# __d(@customer.name) => /// @customer.name = "Joe"
+def __d(expression, binding = nil)
+  value = eval(expression.to_s, binding)
+  __w(value, expression.to_s)
 end
