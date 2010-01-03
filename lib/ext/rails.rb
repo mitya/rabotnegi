f5964bb10c7cdacd6839ddda26c37f94ab2e9888
Dmitry Sokurenko <dmitry@sokurenko.name>
2010-01-03 22:49:19 +0300

    Change the grouped_select to match new actionview implementation

--- lib/ext/rails.rb
+++ lib/ext/rails.rb
@@ -16,23 +16,23 @@ class ActionView::Base
 end
 
 module ActionView::Helpers::FormOptionsHelper
-  def grouped_select(object, method, groups, options = {}, html_options = {})
-    ActionView::Helpers::InstanceTag.new(object, method, self, options.delete(:object)).to_grouped_select_tag(groups, options, html_options)
+  def grouped_select(object, method, grouped_choices, options = {}, html_options = {})
+    ActionView::Helpers::InstanceTag.new(object, method, self, options.delete(:object)).to_grouped_select_tag(grouped_choices, options, html_options)
   end
 end
 
 class ActionView::Helpers::InstanceTag
-  def to_grouped_select_tag(groups, options, html_options)
+  def to_grouped_select_tag(grouped_choices, options, html_options)
     html_options = html_options.stringify_keys
     add_default_name_and_id(html_options)
     value = value(object)
     selected_value = options.has_key?(:selected) ? options[:selected] : value
-    content_tag("select", add_options(@template_object.grouped_options_for_select(groups, selected_value), options, selected_value), html_options)
+    content_tag("select", add_options(grouped_options_for_select(grouped_choices, selected_value), options, selected_value), html_options)
   end
 end
 
 class ActionView::Helpers::FormBuilder
-  def grouped_select(method, choices, options = {}, html_options = {})
-    @template.grouped_select(@object_name, method, choices, objectify_options(options), @default_options.merge(html_options))
+  def grouped_select(method, grouped_choices, options = {}, html_options = {})
+    @template.grouped_select(@object_name, method, grouped_choices, objectify_options(options), @default_options.merge(html_options))
   end
 end
