13ff60d805393d435be3d4a367e0d12231f39247
Dmitry Sokurenko <dmitry@sokurenko.name>
2011-08-06 19:53:11 +0400

    Change the capfile and commit the gemfile.lock

--- Capfile
+++ Capfile
@@ -2,71 +2,63 @@ load 'deploy' if respond_to?(:namespace)
 Dir['vendor/plugins/*/recipes/*.rb'].each { |plugin| load(plugin) }
 Dir['lib/recipes/*.rb'].each { |recipe| load(recipe) }
 
+secrets = YAML.load_file("config/secrets.yml")
+
 set :repository,  "git@sokurenko.unfuddle.com:sokurenko/rabotnegi.git"
 set :deploy_via, :remote_cache
 set :scm, :git
-set :user, "root"
-set :runner, "www-data"
+set :user, "apprunner"
+set :password, credentials["password"]
 set :git_enable_submodules, true
 set :keep_releases, 3
-set :use_sudo, true
+set :use_sudo, false
 
-set :server_env, :demo # ENV['E'] == 'stg' ? :staging : :production
+set :server_env, :staging # ENV['E'] == 'stg' ? :staging : :production
 
-case server_env when :production
-	set :rails_env, :production
-  set :host, 'rabotnegi.ru'
-  set :application, "rabotnegi_prod"
-  set :deploy_to, "/u/rabotnegi_prod"
-when :staging
+# case server_env when :production
+#   set :rails_env, :production
+#   set :host, '178.79.171.92'
+#   set :application, "rabotnegi_prod"
+#   set :deploy_to, "/u/rabotnegi_prod"
+# when :staging
 	set :rails_env, :staging
-  set :host, 'rabotnegi.sokurenko.name'
-	set :ssh_options, {:keys => ["/users/dima/.ssh/id_main"]}
+  set :host, '178.79.171.92'
+  # set :ssh_options, {:keys => ["/users/dima/.ssh/id_rsa"]}
 	set :application, "rabotnegi_stg"
-  set :deploy_to, "/u/rabotnegi_stg"
-when :demo
-	set :rails_env, :staging
-  set :host, "174.143.148.66"
-	# set :ssh_options, {:keys => ["/users/dima/.ssh/id_rsa"]}
-	set :application, "rabotnegi_stg"
-  set :deploy_to, "/u/rabotnegi_stg"
-end
+  set :deploy_to, "/apps/rabotnegi_stg"
+# when :demo
+#   set :rails_env, :staging
+#   set :host, "174.143.148.66"
+#   # set :ssh_options, {:keys => ["/users/dima/.ssh/id_rsa"]}
+#   set :application, "rabotnegi_stg"
+#   set :deploy_to, "/u/rabotnegi_stg"
+# end
 
 server host, :web, :app, :db, :primary => true
 
-set :shared_children, fetch(:shared_children) + %w(sphinx config)
-set :local_git_key_path, "/users/dima/.ssh/id_unfuddle"
-set :git_key_path, "/root/.ssh/id_unfuddle"
-set :git_host_alias, "sokurenko.unfuddle.com"
-set :git_host_name, "sokurenko.unfuddle.com"
+# set :shared_children, fetch(:shared_children) + %w(sphinx config)
+set :sudo_prompt, "sudooo"
 
-set :ssh_config, <<-end
-  Host #{git_host_alias}
-  User git
-  Hostname #{git_host_name}
-  IdentityFile #{git_key_path}
-end
-
-set :logrotate_config, <<-end
-  #{current_path}/log/*.log {
-    daily
-    missingok
-    rotate 9
-    size 1M
-    compress
-    copytruncate
-    notifempty	
-  }
-end
-
-set :passenger_config, <<-end
-  <VirtualHost *:80>
-    ServerName #{host}
-    DocumentRoot #{current_path}/public
-    RailsEnv #{rails_env}
-    ErrorLog  #{current_path}/log/error.log
-    CustomLog #{current_path}/log/access.log common
-  </VirtualHost>
-end
-
-task(:copy_crontab) { run "cp #{current_path}/config/crontab /etc/cron.d/#{application}" }
+# set :logrotate_config, <<-end
+#   #{current_path}/log/*.log {
+#     daily
+#     missingok
+#     rotate 9
+#     size 1M
+#     compress
+#     copytruncate
+#     notifempty  
+#   }
+# end
+# 
+# set :passenger_config, <<-end
+#   <VirtualHost *:80>
+#     ServerName #{host}
+#     DocumentRoot #{current_path}/public
+#     RailsEnv #{rails_env}
+#     ErrorLog  #{current_path}/log/error.log
+#     CustomLog #{current_path}/log/access.log common
+#   </VirtualHost>
+# end
+# 
+# task(:copy_crontab) { run "cp #{current_path}/config/crontab /etc/cron.d/#{application}" }
--- lib/recipes/installation.rb
+++ lib/recipes/installation.rb
@@ -1,46 +1,55 @@
 default_run_options[:pty] = true # required for the first time repo access to answer "yes"
 
 def run_rake(task, params = "", options = {})
-  options[:sudo] ||= runner
-	sudo_modifier = "#{sudo :as => options[:sudo]}" if options[:sudo]
-	run "cd #{current_path} && #{sudo_modifier} RAILS_ENV=#{rails_env} rake #{task} #{params}"
+  sudo_modifier = "#{sudo :as => options[:sudo]}" if options[:sudo]
+  run "cd #{current_path} && #{sudo_modifier} RAILS_ENV=#{rails_env} rake #{task} #{params}"
 end
 
-namespace :deploy do
-  task(:start)   { }  
-  task(:stop)    { }
-  task(:restart) { run "touch #{current_path}/tmp/restart.txt" }
-end
+# namespace :deploy do
+#   task(:start)   { }  
+#   task(:stop)    { }
+#   task(:restart) { run "touch #{current_path}/tmp/restart.txt" }
+# end
 
-task(:log) { puts capture("tail -n #{ENV['N'] || 200} #{current_path}/log/#{rails_env}.log") }
-task(:rake) { run_rake ENV['T'], ENV['P'] }
-task(:rake_as_runner) { run_rake ENV['T'], ENV['P'], :sudo => runner }
+# task(:log) { puts capture("tail -n #{ENV['N'] || 200} #{current_path}/log/#{rails_env}.log") }
+# task(:rake) { run_rake ENV['T'], ENV['P'] }
+# task(:rake_as_runner) { run_rake ENV['T'], ENV['P'], :sudo => runner }
+# task(:fix_permissions) { run "chown -R #{runner}:#{runner} #{current_path}/ #{shared_path}/log #{shared_path}/pids" }
 
-task(:fix_permissions) { run "chown -R #{runner}:#{runner} #{current_path}/ #{shared_path}/log #{shared_path}/pids" }
-after 'deploy:migrate', 'fix_permissions'
-after 'deploy:update', 'fix_permissions'
-
-set :ssh_config_path, "/root/.ssh/config"
-
+# after 'deploy:migrate', 'fix_permissions'
+# after 'deploy:update', 'fix_permissions'
+# 
 namespace :deploy do
   namespace :install do
     task :default do
       setup
       git
-    	update
-      db
-      migrate
-      passenger
-      logrotate
+      gems
+      # update
+      # db
+      # migrate
+      # passenger
+      # logrotate
     end
 
     task :git do
-      top.upload local_git_key_path, git_key_path, :mode => "600"
-      sudo "touch #{ssh_config_path}"
-      sudo "chmod 600 #{ssh_config_path}"
+      git_host = "sokurenko.unfuddle.com"
+      git_key_path = "/home/#{user}/.ssh/id_git_#{application}"
+      ssh_config_path = "/home/#{user}/.ssh/config"
+      
+      top.upload "/users/dima/.ssh/id_unfuddle", git_key_path, :mode => "600"
+      run "touch #{ssh_config_path}"
+      run "chmod 600 #{ssh_config_path}"
 
-      entry_delimiter = "## config for #{git_host_alias}"
-      entry = "\n" + entry_delimiter + "\n" + ssh_config + entry_delimiter + "\n"
+      ssh_config = <<-end
+        Host #{git_host}
+        User git
+        Hostname #{git_host}
+        IdentityFile #{git_key_path}
+      end
+
+      entry_delimiter = "## config for #{git_host}"
+      entry = entry_delimiter + "\n" + ssh_config + entry_delimiter + "\n"
       
       current_config = capture "cat #{ssh_config_path}"
       current_config.gsub! /(\n|\r)+/, "\n" # there is a bunch of "\r" here
@@ -49,30 +58,34 @@ namespace :deploy do
       put current_config + entry, ssh_config_path
     end
 
+    task :gems do
+      run "cd #{current_path} && #{sudo} bundle"
+    end
+
     task :db do
       run_rake "db:create"
     end
-
-    task :logrotate do
-      put logrotate_config, "/etc/logrotate.d/#{application}" 
-    end
     
-    task :passenger do
-      put passenger_config, "/etc/apache2/sites-available/#{application}"
-      sudo "a2ensite #{application}"
-      sudo "/etc/init.d/apache2 reload"
-    end
-
-    def append_text(text, file)
-      # text = text.gsub("'", "'\\\\''").gsub("\n", '\n')
-      # run "echo -e '#{text}' | tee -a #{file}"
-      # run "tee -a #{file}", :data => text
-      # run "cat >> #{file}", :data => text
-      ("\n" + text + "\n").each_line { |line| run %(echo "#{line}" >> #{file}) }
-    end
+    # task :logrotate do
+    #   put logrotate_config, "/etc/logrotate.d/#{application}" 
+    # end
+    # 
+    # task :passenger do
+    #   put passenger_config, "/etc/apache2/sites-available/#{application}"
+    #   sudo "a2ensite #{application}"
+    #   sudo "/etc/init.d/apache2 reload"
+    # end
+    # 
+    # def append_text(text, file)
+    #   # text = text.gsub("'", "'\\\\''").gsub("\n", '\n')
+    #   # run "echo -e '#{text}' | tee -a #{file}"
+    #   # run "tee -a #{file}", :data => text
+    #   # run "cat >> #{file}", :data => text
+    #   ("\n" + text + "\n").each_line { |line| run %(echo "#{line}" >> #{file}) }
+    # end
   end
 end
-
-before "deploy:setup" do
-	sudo "mkdir -p /u"
-end
+# 
+# before "deploy:setup" do
+#   sudo "mkdir -p /apps"
+# end
